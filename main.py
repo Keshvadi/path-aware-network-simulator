d92ccc30406927894f9d04705ee6aee2a53d9311
heyitsmeali <alissab@posteo.de>
2025-06-20 15:02:45 -0700

    basic outline

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+
+
+class Path: 
+    def __init__(self):
+        pass
+
+
+
+class Agent: 
+    def __init__(self):
+        pass 
+
+
+
+
+def load_topology(json):
+    pass
+
+def parse_agent():
+    pass 
+
+
+def parse_paths():
+    pass 
+
+
+
+if __name__ == "__main__":
+    print("") 
